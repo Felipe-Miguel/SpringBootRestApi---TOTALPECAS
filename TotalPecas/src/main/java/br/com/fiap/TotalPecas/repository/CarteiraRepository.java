e39241f8ec91ee9d151fdbca6a7d09715d47f27f
Felipe Miguel Ortega <83983464+Felipe-Miguel@users.noreply.github.com>
2023-04-10 14:29:28 -0300

    Delete TotalPecas directory

--- TotalPecas/src/main/java/br/com/fiap/TotalPecas/repository/CarteiraRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.TotalPecas.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.fiap.TotalPecas.models.Carteira;
-
-public interface CarteiraRepository extends JpaRepository<Carteira, Long>{
-}
